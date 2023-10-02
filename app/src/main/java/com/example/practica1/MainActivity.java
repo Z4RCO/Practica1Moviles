7307fb89a9b5107666de4b4e0b4b46496d535a6f
Z4RCO <z3rc0mrm@gmail.com>
2023-10-02 17:20:36 +0200

    Cambios diseño menú

--- app/src/main/java/com/example/practica1/MainActivity.java
+++ app/src/main/java/com/example/practica1/MainActivity.java
@@ -2,19 +2,46 @@ package com.example.practica1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayer musica;
+    private boolean activo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        musica = MediaPlayer.create(this, R.raw.gunshot);
+        musica.setLooping(true);
+        musica.start();
+        activo = true;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
     public void jugar(View view){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.correcto);
+        mp.start();
 
+        Intent intent = new Intent(this, PreguntasActivity.class);
+        startActivity(intent);
     }
+
+    public void cambiarMusica(View view){
+        if(activo){
+            musica.pause();
+            activo = false;
+        }
+        else{
+            musica.start();
+            activo = true;
+        }
+    }
+
+    //TODO Parar música cuando cambie la Activity
+
 }
--- app/src/main/java/com/example/practica1/PreguntasActivity.java
+++ app/src/main/java/com/example/practica1/PreguntasActivity.java
@@ -2,13 +2,26 @@ package com.example.practica1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PreguntasActivity extends AppCompatActivity {
+    private int puntos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preguntas);
+
+        puntos = 0;
     }
+
+    public void salir(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("puntos", puntos);
+
+        startActivity(intent);
+    }
+
 }
