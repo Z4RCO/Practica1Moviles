df8f1e2f4fd4f6402d022ade40c9b7ef375d946f
Z4RCO <z3rc0mrm@gmail.com>
2023-10-09 17:29:35 +0200

    fixed mute button and music

--- app/src/main/java/com/example/practica1/MainActivity.java
+++ app/src/main/java/com/example/practica1/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.practica1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
@@ -14,62 +11,74 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     private MediaPlayer musica;
+    private MediaPlayer tucan;
     private boolean activo = true;
+    private int silencio = R.drawable.silencio;
+    private ImageButton boton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        musica = MediaPlayer.create(this, R.raw.wildquest);
-
-        musica.setLooping(true);
-
-        Intent intent = getIntent();
-
-        activo = intent.getBooleanExtra("musica",true);
-
-
-        if(activo){
-            musica.start();
-        }
-
-
+        System.out.println("Create Main");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        tucan = MediaPlayer.create(this,R.raw.cuervo);
+        musica = MediaPlayer.create(this, R.raw.wildquest);
+        musica.setLooping(true);
+
+        System.out.println("Start Main");
+        Intent intent = getIntent();
+
+        boton = findViewById(R.id.Silenciar);
+        if(boton == null) System.out.println("BOTON ES NULL");
+        activo = intent.getBooleanExtra("musica", true);
+        if(activo){
+            System.out.println("hay musica");
+            musica.start();
+        }
+        else{
+            System.out.println("No hay musica");
+            boton.setImageResource(R.drawable.silencio);
+        }
+    }
+
     public void jugar(View view){
         MediaPlayer mp = MediaPlayer.create(this,R.raw.correcto);
         mp.start();
 
         Intent intent = new Intent(this, PreguntasActivity.class);
         intent.putExtra("musica",activo);
-        musica.pause();
+        if(musica.isPlaying())musica.pause();
         startActivity(intent);
+
     }
 
     public void salir(View view){
-        musica.stop();
         finishAffinity();
     }
 
     public void sonidoTucan(View view){
-        MediaPlayer mp = MediaPlayer.create(this,R.raw.cuervo);
-        mp.start();
+        tucan.start();
     }
 
     public void cambiarMusica(View view){
         ImageButton boton = (ImageButton) view;
         if(activo){
-            musica.pause();
+            if(musica.isPlaying())musica.pause();
             activo = false;
             boton.setImageResource(R.drawable.silencio);
-
-
         }
         else{
-            musica.start();
+            if(!musica.isPlaying())musica.start();
             activo = true;
             boton.setImageResource(R.drawable.sonido);
-
         }
     }
 
@@ -78,6 +87,16 @@ public class MainActivity extends AppCompatActivity {
         super.onBackPressed();
         finishAffinity();
     }
+
+    @Override
+    protected void onStop() {
+        System.out.println("Stop Main");
+        super.onStop();
+    }
+
+
+
+
 
     //TODO Hacer que el boton coincida con el sonido al volver al main
 
