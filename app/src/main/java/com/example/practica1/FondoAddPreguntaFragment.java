906bbfd3c442aa2232af89ca4041ea43aadbaaf7
crist <cristianyanez999@gmail.com>
2023-11-27 09:23:12 +0100

    varios cambios y manual completado

--- app/src/main/java/com/example/practica1/FondoAddPreguntaFragment.java
+++ app/src/main/java/com/example/practica1/FondoAddPreguntaFragment.java
@@ -13,37 +13,17 @@ public class FondoAddPreguntaFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-   private String mParam1;
-    private String mParam2;
 
     public FondoAddPreguntaFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FondoAddPreguntaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FondoAddPreguntaFragment newInstance(String param1, String param2) {
-        FondoAddPreguntaFragment fragment = new FondoAddPreguntaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            getArguments().getString(ARG_PARAM1);
+            getArguments().getString(ARG_PARAM2);
         }
     }
 
--- app/src/main/java/com/example/practica1/FondoManualFragment.java
+++ app/src/main/java/com/example/practica1/FondoManualFragment.java
@@ -14,37 +14,16 @@ public class FondoManualFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private String mParam1;
-    private String mParam2;
-
     public FondoManualFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FondoManualFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FondoManualFragment newInstance(String param1, String param2) {
-        FondoManualFragment fragment = new FondoManualFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            getArguments().getString(ARG_PARAM1);
+            getArguments().getString(ARG_PARAM2);
         }
     }
 
